aea15ef4910d57d29791dd119b0970caacbcc31c
Dilan Shah <dilanshah@Dilans-MacBook-Air.local>
2025-10-15 08:41:38 -0400

    created first scroll art with 3 symbols that randomly are drawn at random areas.

--- scrollart1/src/App.java
+++ scrollart1/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class App {
+        static final String sign1 = "*";
+        static final String sign2 = "+";
+        static final String sign3 = "#";
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Random rand = new Random();
+        int num = rand.nextInt(3);
+        while(true){
+            int numSpaces = rand.nextInt(120);
+            if(num == 0){
+            System.out.print(sign1);
+            for(int s = 0; s < numSpaces; s++){
+            }
+            } else if(num == 1){
+                System.out.print(sign2);
+            } else{
+                System.out.print(sign3);
+            }
+            for(int s = 0; s < numSpaces; s++){
+                System.out.print(" ");
+            }
+            num = rand.nextInt(3);
+        }
     }
 }
